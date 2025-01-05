f9a8cd78e423f4829803612cdeace2f1beb1e5ba
hema <attotihemalatha08@gmail.com>
2025-01-05 21:01:10 +0530

    count words

--- count_words/countwords.sh
+++ count_words/countwords.sh
@@ -2,7 +2,7 @@
 
 #text_file="My name is Hemalatha. Hemalatha is my name. My daughter name is Aadhya"
 
-read $text_file
+text_file-File=$(read textfile)
 
 WORDS=$(echo $text_file | cut -d " " -f1)
 
