afb3175a55617d015b46054bf79aa02d455bf73b
hema <attotihemalatha08@gmail.com>
2025-01-05 08:48:07 +0530

    mysqlserver

--- expense/mysql.sh
+++ expense/mysql.sh
@@ -8,9 +8,18 @@ Y="\e[33m"
 N="\e[0m"
 
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 LOGS_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIME_STAMP.log"
+
+VALIDATE(){
+    if [ $? -ne 0]
+    then
+        echo -e "$2 is $R FAILURE $N"
+    else
+        echo -e "$2 is $G SUCCESS $N"
+    fi
+}
 
 echo "Script started running at $TIME_STAMP"
 
@@ -18,11 +27,21 @@ CHECK_USER
 
 CHECK_USER(){
 
-    if [ $USER_ID -ne 0]
+    if [ $USER_ID -ne 0 ]
         then
         echo "ERROR:: You need to be sudo user to run the script"
         exit 1
-    else
-
     fi   
 }
+
+dnf install mysql -y &>>LOGS_FILE_NAME
+VALIDATE $? "Installing MySQL"
+
+systemctl enable mysqld
+VALIDATE $? "Enabling mysqld service"
+
+systemctl start mysqld
+VALIDATE $? "Starting mysqld service"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
