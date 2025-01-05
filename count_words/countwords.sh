e1f577c972df851dac1da3e939f6f17386ec67c9
hema <attotihemalatha08@gmail.com>
2025-01-05 22:00:42 +0530

    file transpose

--- count_words/countwords.sh
+++ count_words/countwords.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-#text_file="My name is Hemalatha. Hemalatha is my name. My daughter name is Aadhya"
+text_file="My name is Hemalatha. Hemalatha is my name. My daughter name is Aadhya"
 
-text_file=$(read textfile)
-
-WORDS=$(cut -d " " -f1 $text_file)
-
-echo $WORDS
